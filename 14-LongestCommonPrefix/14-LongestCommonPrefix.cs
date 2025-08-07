d3239d03c9e789e69e00a57cc77787f1b4e07a85
Daniel Marcondes <103604404+Ioahim@users.noreply.github.com>
2025-08-07 18:42:08 -0300

    Create 14-LongestCommonPrefix

---
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.cs
@@ -0,0 +1,18 @@
+// Last updated: 07/08/2025, 18:42:10
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        string prefix = "";
+
+        for(int i = 0; i < strs[0].Length; i++){
+            char c = strs[0][i];
+            for(int j = 1; j < strs.Length; j++){
+                if(i >= strs[j].Length || strs[j][i] != c){
+                    return prefix;
+                }
+            }
+            prefix += c;
+        }
+
+        return prefix;
+    }
+}
